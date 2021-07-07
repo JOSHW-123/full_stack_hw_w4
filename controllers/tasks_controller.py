bc37ef410ef9db869536dda1b3269bc619691fd1
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-07-07 18:17:52 +0100

    First Commit

---
+++ controllers/tasks_controller.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from werkzeug.utils import redirect
+from repositories import task_repository, user_repository
+from models.task import Task
+
+tasks_blueprint = Blueprint("tasks", __name__)
+
+@tasks_blueprint.route("/tasks")
+def tasks():
+    tasks = task_repository.select_all()
+    return render_template("tasks/index.html", all_tasks=tasks)
+
+@tasks_blueprint.route("/tasks/new")
+def new_task():
+    users = user_repository.select_all()
+    return render_template("tasks/new.html", all_users=users)
+
+@tasks_blueprint.route("/tasks", methods=["POST"])
+def create_task():
+    description = request.form["description"]
+    duration = request.form["duration"]
+    completed = request.form["completed"]
+    user_id = request.form["user_id"]
+    user = user_repository.select(user_id)
+    new_task = Task(description, user, duration, completed)
+    task_repository.save(new_task)
+    return redirect("/tasks")
+
+
+# @tasks_blueprint.route("/tasks", methods=["POST"])
+# def create_task():
+#     print(request.form)
+# This will show immutable object in terminal
+
+@tasks_blueprint.route("/tasks/<id>/delete", methods=["POST"])
+def delete_task(id):
+    task_repository.delete(id)
+    return redirect("/tasks")
+
+@tasks_blueprint.route("/tasks/<id>", )
